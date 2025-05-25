bace0f5c50e8480fade149c09fe37c5237929b30
danieldanty <dynosiro44@gmail.com>
2025-05-25 11:38:27 +0700

    Add files via upload

---
+++ Parfume/parfumelist/Parfum.php
@@ -0,0 +1,78 @@
+<?php
+class Parfum {
+    private $conn;
+    private $table_name = "parfums";
+
+    public $ParfumID;
+    public $Name;
+    public $Price;
+    public $Gender;
+    public $Volume;
+    public $ImageURL;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Create a new parfum
+    public function create() {
+        $query = "INSERT INTO " . $this->table_name . " (Name, Price, Gender, Volume, ImageURL) VALUES (:Name, :Price, :Gender, :Volume, :ImageURL)";
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':Name', $this->Name);
+        $stmt->bindParam(':Price', $this->Price);
+        $stmt->bindParam(':Gender', $this->Gender);
+        $stmt->bindParam(':Volume', $this->Volume);
+        $stmt->bindParam(':ImageURL', $this->ImageURL);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function readAll() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function readSingle($id) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE ParfumID = :ParfumID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':ParfumID', $id);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function update() {
+        $query = "UPDATE " . $this->table_name . " SET Name = :Name, Price = :Price, Gender = :Gender, Volume = :Volume, ImageURL = :ImageURL WHERE ParfumID = :ParfumID";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(':ParfumID', $this->ParfumID);
+        $stmt->bindParam(':Name', $this->Name);
+        $stmt->bindParam(':Price', $this->Price);
+        $stmt->bindParam(':Gender', $this->Gender);
+        $stmt->bindParam(':Volume', $this->Volume);
+        $stmt->bindParam(':ImageURL', $this->ImageURL);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE ParfumID = :ParfumID";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':ParfumID', $id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+}
+?>
